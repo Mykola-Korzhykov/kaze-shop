42861a0bb1bc4b50a0a56d53ef4b02f450b95760
Pavlo <sniedalx@gmail.com>
2023-02-20 10:32:48 -0800

    20 Feb

---
+++ src/components/screens/Main/BuyProducts/SliderProducts/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import s from './SliderProducts.module.scss'
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+//store 
+import { useSelector } from 'react-redux';
+import { RootState } from '@/redux/store';
+import Product from '../Product';
+
+
+const settings = {
+    dots: false,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+        {
+            breakpoint: 1200, // настройки для планшета
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                speed: 600,
+            }
+        },
+    ]
+
+
+};
+
+const SliderProducts = () => {
+
+    const productsLeggings = useSelector((state: RootState) => state.main?.productsLeggings)
+
+
+    return (
+        <div>
+
+            <Slider className={s.wrapper} {...settings}>
+
+                {/* <button className={s.btn}>
+                click
+            </button> */}
+                {productsLeggings?.map((obj) => {
+                    return <Product key={obj.id} price={obj.price} description={obj.description} img={obj.img} id={obj.id} />
+                })}
+            </Slider>
+
+            <div className={s.btn_catalog}>
+                В каталог
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M3.75 12H20.25" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M13.5 5.25L20.25 12L13.5 18.75" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+
+
+                {/* <svg className={s.arrow_svg} width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5 16H27" stroke="#0B0B0B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M18 7L27 16L18 25" stroke="#0B0B0B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg> */}
+
+            </div>
+        </div>
+
+    );
+}
+
+export default SliderProducts;
--- src/components/screens/Main/Main.tsx
+++ src/components/screens/Main/Main.tsx
@@ -3,64 +3,20 @@ import s from './Main.module.scss'
 import Image from 'next/image';
 import { useSelector } from 'react-redux';
 import { RootState } from '@/redux/store';
-import Product from './Product'
+import Product from './BuyProducts/Product'
 //imgs
+import { CSSTransition } from 'react-transition-group';
 
 import product1 from '../../../assets/images/main/products/product1.svg'
 import product2 from '../../../assets/images/main/products/product2.svg'
 import product3 from '../../../assets/images/main/products/product3.svg'
+import BuyProducts from './BuyProducts';
 
 const Main = () => {
 
-
-	const productsLeggings = useSelector((state: RootState) => state.main?.productsLeggings)
-	let [slide, setSlide] = React.useState(0)
-
-	console.log('productsLeggings', productsLeggings)
 	return (
-
 		<div className={s.wrapper}>
-			<div className={s.inner__wrapper}>
-				<div className={s.inner_nav}>
-					<span className={s.nav_text_1}>Повседневное  белье</span>
-					<span className={s.nav_text_2}>Велосипедки</span>
-					<button className={s.nav_btn}>
-						купить
-						<svg className={s.arrow} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M3.75 12H20.25" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-							<path d="M13.5 5.25L20.25 12L13.5 18.75" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-						</svg>
-					</button>
-				</div>
-				<div className={s.inner_text}>
-					<span className={s.text}>
-						<span className={s.transparent_text}>БУДЬ</span>
-						<span className={s.white_text}>СОБОЙ</span>
-					</span>
-				</div>
-				<div className={s.inner_collection}>
-
-					<button onClick={() => {
-						// console.log(slide)
-						setSlide(slide === 210 * (productsLeggings.length - 3) ? 0 : slide + 210)
-					}} className={s.btn}>
-						<svg className={s.btn_arrow} width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-							<path d="M5 16H27" stroke="#0B0B0B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-							<path d="M18 7L27 16L18 25" stroke="#0B0B0B" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-						</svg>
-					</button>
-
-					<div className={s.collection_wrapper}>
-						<div className={s.products_wrapper} style={{ transform: `translateX(-${slide}px)` }}>
-							{productsLeggings?.map((obj) => {
-								return <Product key={obj.id} price={obj.price} description={obj.description} img={obj.img} id={obj.id} />
-							})}
-						</div>
-
-					</div>
-
-				</div>
-			</div>
+			<BuyProducts />
 		</div>
 
 	)
