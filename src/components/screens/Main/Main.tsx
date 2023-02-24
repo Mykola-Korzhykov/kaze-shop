fa5cd42b5a267908a6dae19e4c314acf68e26f40
SniedaPavlo <sniedalx@gmail.com>
2023-02-24 13:58:40 +0200

    main contain BuyProducts

--- src/components/screens/Main/Main.tsx
+++ src/components/screens/Main/Main.tsx
@@ -22,7 +22,7 @@ const Main = () => {
 
 	return (
 		<div className={s.wrapper}>
-			{/* <BuyProducts /> */}
+			<BuyProducts /> 
 			{/* <ProductFitnes /> */}
 			{/* <About /> */}
 			{/* <ProductsAccessories />  */}
