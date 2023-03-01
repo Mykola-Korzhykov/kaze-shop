31f92d9ebaa58654e1617df97b8daa3b38429258
SniedaPavlo <sniedalx@gmail.com>
2023-03-01 13:29:52 +0200

    fixed add-product mobile props inside <InputTextItem />

--- src/pages/admin-settings/add-product/index.tsx
+++ src/pages/admin-settings/add-product/index.tsx
@@ -11,13 +11,16 @@ const AddProduct = () => {
     // const inputs = useSelector((state: RootState) => state.admin.inputs)
 
     const  [inputs, setInputs] = React.useState([
-        { id: 0, type: 'text', text: 'Название товара RU', placeholder: 'Введите название кнопки', label: 'text' },
-        { id: 1, type: 'text', text: 'Название товара UA', placeholder: 'Введите название кнопки', label: 'text' },
-        { id: 2, type: 'text', text: 'Название товара SRB', placeholder: 'Введите название кнопки', label: 'text' },
-        { id: 3, type: 'text', text: 'Название товара ENG', placeholder: 'Введите название кнопки', label: 'text' },
-        { id: 5, type: 'select', text: 'Категория товара', placeholder: 'Выберите категорию товара ', label: 'text' },
-        { id: 6, type: 'text', text: 'Цена в долларах', placeholder: 'Введите цену', label: 'text' },
-        { id: 7, type: 'text', text: 'Количество товара', placeholder: 'Введите количество товаров', label: 'text' }
+        { id: 0, type: 'text', text: 'Название товара RU', placeholder: 'Введите название кнопки', label: 'text', disable: false },
+        { id: 1, type: 'text', text: 'Название товара UA', placeholder: 'Введите название кнопки', label: 'text', disable: false },
+        { id: 2, type: 'text', text: 'Название товара SRB', placeholder: 'Введите название кнопки', label: 'text',  disable: false },
+        { id: 3, type: 'text', text: 'Название товара ENG', placeholder: 'Введите название кнопки', label: 'text',disable: false },
+        { id: 5, type: 'select', text: 'Категория товара', placeholder: 'Выберите категорию товара ', label: 'text', disable: false },
+        { id: 6, type: 'text', text: 'Цена в долларах', placeholder: 'Введите цену', label: 'text', disable: false },
+        { id: 7, type: 'text', text: 'Количество товара', placeholder: 'Введите количество товаров', label: 'text', disable: false },
+        // { id: 8, type: 'text', text: 'Цвет', placeholder: 'Выбрать один цвет фотографии', label: 'text', disable: true, colors: colors },
+        //{ id: 9, type: 'text', text: 'Выберите существующий товар', placeholder: 'Выберите существующий товар', label: 'text', disable: true },
+   
     ])
 
     const inputsFistWrapper_1 = inputs?.slice(0, 2)
@@ -41,22 +44,47 @@ const AddProduct = () => {
                         <div className={s.first_wrapper}>
                             <div className={s.first_wrapper_1}>
                                 {inputsFistWrapper_1?.map((obj) => {
-                                    return <InputTextItem key={obj.id} id={obj.id} type={obj.type} text={obj.text} placeholder={obj.placeholder} />
+                                    return <InputTextItem
+                                    disable={obj.disable} 
+                                    key={obj.id} id={obj.id} 
+                                    type={obj.type} 
+                                    text={obj.text} 
+                                    placeholder={obj.placeholder}
+                                   
+                                    />
                                 })}
                             </div>
                             <div className={s.first_wrapper_2}>
                                 {inputsFistWrapper_2?.map((obj) => {
-                                    return <InputTextItem key={obj.id} id={obj.id} type={obj.type} text={obj.text} placeholder={obj.placeholder} />
+                                    return <InputTextItem 
+                                    disable={obj.disable} 
+                                    key={obj.id} id={obj.id} 
+                                    type={obj.type} 
+                                    text={obj.text} 
+                                    placeholder={obj.placeholder}
+                                    />
                                 })}
                             </div>
                             <div className={s.first_wrapper_2}>
                                 {inputsFistWrapper_3?.map((obj) => {
-                                    return <InputTextItem key={obj.id} id={obj.id} type={obj.type} text={obj.text} placeholder={obj.placeholder} />
+                                    return <InputTextItem
+                                    disable={obj.disable} 
+                                    key={obj.id} id={obj.id} 
+                                    type={obj.type} 
+                                    text={obj.text} 
+                                    placeholder={obj.placeholder}
+                                    />
                                 })}
                             </div>
                             <div className={s.first_wrapper_2}>
                                 {inputsFistWrapper_4?.map((obj) => {
-                                    return <InputTextItem key={obj.id} id={obj.id} type={obj.type} text={obj.text} placeholder={obj.placeholder} />
+                                    return <InputTextItem 
+                                    disable={obj.disable} 
+                                    key={obj.id} id={obj.id} 
+                                    type={obj.type} 
+                                    text={obj.text} 
+                                    placeholder={obj.placeholder}
+                                    />
                                 })}
                             </div>
 
