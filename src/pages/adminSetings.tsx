4917a0dfb97b954d3e495ccaf092940d0d014a26
SniedaPavlo <sniedalx@gmail.com>
2023-03-07 21:41:45 +0200

    fixed build bag (found page without a React Component as default export in pages/adminSetings)

--- src/pages/adminSetings.tsx
+++ src/pages/adminSetings.tsx
@@ -1,22 +1,39 @@
 // import Cabinet from '@/components/screens/Cabinet/Cabinet'
-import React from "react";
-// import SpinnerLayout from '@/layouts/SpinnerLayout'
-// import { wrapper } from '@/redux/store'
-// import { addUserInfo } from '@/redux/slices/user'
-// import { withAuth } from '@/hoc/RequiredAuth'
-// import { NextPage } from 'next'
-// import { Api } from '@/services'
-// import axios from 'axios'
-// import AdminSetings from '@/components/screens/Cabinet/CabinetAdmin/AdminSetingsModile/AdminSetings'
+// import React from "react";
+//  import SpinnerLayout from '@/layouts/SpinnerLayout'
+//  import { wrapper } from '@/redux/store'
+//  import { addUserInfo } from '@/redux/slices/user'
+//  import { withAuth } from '@/hoc/RequiredAuth'
+//  import { NextPage } from 'next'
+//  import { Api } from '@/services'
+//  import axios from 'axios'
+ //import AdminSetings from '@/components/screens/Cabinet/CabinetAdmin/AdminSetingsModile/AdminSetings'
 // import AdminSetings from '../components/screens/Cabinet/CabinetAdmin/'
 
 
 // const adminSetings: NextPage = () => {
 //     return (
 //         <SpinnerLayout>
-//             <AdminSetings />
+//             <div className="pop"> d</div>
+//            {/* <AdminSetings /> */}
 //         </SpinnerLayout>
 //     )
 // }
 
 // export default adminSetings
+
+
+import React from "react";
+import SpinnerLayout from '@/layouts/SpinnerLayout'
+//import AdminSetings from '@/components/screens/Cabinet/CabinetAdmin/AdminSetingsModile/AdminSetings'
+
+const adminSetings = () => {
+    return (
+        <SpinnerLayout>
+            <div className="pop"> d</div>
+            {/* <AdminSetings /> */}
+        </SpinnerLayout>
+    )
+}
+
+export default adminSetings;
