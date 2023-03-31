54a449a58f0f7003bc3574fd16ff3cb6860f22cf
AlexandrM <chestermen30@gmail.com>
2023-04-01 01:05:31 +0300

    fix

--- next.config.js
+++ next.config.js
@@ -9,17 +9,12 @@ const nextConfig = {
 		buildActivity: false,
 	},
 	images: {
-<<<<<<< HEAD
 		// domains: ['[::1]'],
 		loader: 'custom',
 		loaderFile: './src/utils/customImgLoader.ts',
 	},
 };
 
-=======
-	domains: ['api.kaze-shop.online'],
-},
->>>>>>> ae331c1bfab4ea5aeef3c7a4c18042ab9d0a803c
 
 
 module.exports = nextConfig
